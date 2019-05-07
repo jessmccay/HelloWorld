84ccfd6dc7ead4cdbff817ef1d4656c3aee6b719
jessmccay <jessmccay@gmail.com>
2019-05-07 11:31:59 -0400

    safe null checks

--- src/app.kt
+++ src/app.kt
@@ -2,23 +2,23 @@ import com.rsk.Person
 
 fun main() {
     println ("Hello, World!")
-
     val brixton = Person("Brixton")
-
     brixton.name = "Jessica"
-
     println("Your name is ${brixton.name}")
-
     brixton.display()
-
     brixton.displayWithLambda(::printName)
 
-    val question = Question()
-    question.Answer = "happiness"
-    question.display()
-    println("hey ${question.Answer}")
-    printName("The answer to the question, ${question.Question}, is ${question.Answer}.")
-
+    val q:Question? = Question()
+    println(q?.question)
+    q?.answer = "Love & happiness"
+    q?.display()
+    val message = if(q?.answer == q?.correctAnswer) {
+        "You were right!"
+    } else {
+        "Sorry, you were incorrect."
+    }
+    println(message)
+    println("The answer to the question, ${q?.question}, is ${q?.answer}.")
 }
 fun printName(name: String){
     println(name)
@@ -26,10 +26,13 @@ fun printName(name: String){
 }
 
 class Question {
-    var Answer:String = ""
-    val Question:String = "What is the meaning of life?"
+    var answer:String? = null
+    val question:String = "What is the meaning of life?"
+    val correctAnswer = "Love & happiness"
+
+
     fun display() {
-        println("You said $Answer")
+        println("You said $answer")
     }
 
 }
