b04641e14b5fa85bf83bc64966719cf6933ca8b3
jessmccay <jessmccay@gmail.com>
2019-05-07 10:19:52 -0400

    lambda function

--- src/app.kt
+++ src/app.kt
@@ -4,7 +4,15 @@ fun main() {
     println ("Hello, World!")
 
     val brixton = Person("Brixton")
+
     brixton.name = "Jessica"
+
     println("Your name is ${brixton.name}")
 
+    brixton.display()
+
+    brixton.displayWithLambda(::printName)
+}
+fun printName(name: String){
+    println(name)
 }
--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,5 +1,11 @@
 package com.rsk
 
 class Person (var name: String){
+    fun display(){
+         println("Display: $name")
+    }
 
+    fun displayWithLambda(func: (s:String) -> Unit){
+        func(name)
+    }
 }
